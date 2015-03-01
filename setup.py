97f755f1e893095106488a79ade668ceae5faf70
MrS0m30n3 <ytubedlg@gmail.com>
2015-03-01 14:06:24 +0200

    Add LICENSE & README on source distribution

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 
 
 setup(
-    author      = 'Sotiris Papadopoulos',
-    name        = 'twodict',
-    description = 'Simple two way ordered dictionary for Python',
-    version     = '1.0',
-    license     = 'Unlicense',
-    url         = 'https://github.com/MrS0m30n3/twodict/',
-    py_modules  = ['twodict']
+    author       = 'Sotiris Papadopoulos',
+    author_email = 'ytubedlg@gmail.com',
+    name         = 'twodict',
+    description  = 'Simple two way ordered dictionary for Python',
+    version      = '1.0',
+    license      = 'Unlicense',
+    url          = 'https://github.com/MrS0m30n3/twodict/',
+    py_modules   = ['twodict']
 )
